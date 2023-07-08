8086aecc7986a5e248c4d85054cea206ef6650f0
Linda Givers <lindagivers@gmail.com>
2023-07-08 12:56:30 -0400

    1st commit

---
+++ javascript.js
@@ -0,0 +1,2 @@
+console.log("If a is success in life, I should say that the formula is a equals x plus y plus z, x being work and y being play. Z is keeping your mouth shut. - Albert Einstein");
+    
